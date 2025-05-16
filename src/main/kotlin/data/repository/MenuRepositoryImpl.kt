d31649aec7791fe44a9083c920cbaf9514b4cae6
Eddie-Pak <foalstn@naver.com>
2025-05-16 17:34:45 +0900

    feat: MenuRepository 구현

--- src/main/kotlin/data/repository/MenuRepositoryImpl.kt
+++ src/main/kotlin/data/repository/MenuRepositoryImpl.kt
@@ -1,4 +1,52 @@
 package data.repository
 
-class MenuRepositoryImpl {
+import data.datasource.MenuDataSource
+import domain.model.Menu
+import domain.repository.MenuRepository
+
+class MenuRepositoryImpl(private val dataSource: MenuDataSource) : MenuRepository {
+    override fun getMenuList(): List<Menu> = dataSource.getMenuData().map { it.toDomain() }
+
+    override fun addMenu(name: String, price: Int): Menu {
+        val menuDtoList = dataSource.getMenuData().toMutableList()
+
+        val newId = if (menuDtoList.isEmpty()) 1 else menuDtoList.maxOf { it.id } + 1
+
+        val newMenu = Menu(
+            id = newId,
+            name = name,
+            price = price
+        )
+
+        menuDtoList.add(newMenu.toDTO())
+
+        dataSource.saveMenuData(menuDtoList)
+
+        return newMenu
+    }
+
+    override fun updateMenu(id: Int, price: Int): Menu {
+        val menuDtoList = dataSource.getMenuData().toMutableList()
+
+        val index = menuDtoList.indexOfFirst { it.id == id }
+        val updateMenu = menuDtoList[index].toDomain().copy(price = price)
+
+        menuDtoList[index] = updateMenu.toDTO()
+
+        dataSource.saveMenuData(menuDtoList)
+
+        return updateMenu
+    }
+
+    override fun deleteMenu(id: Int): Boolean {
+        val menuDtoList = dataSource.getMenuData().toMutableList()
+
+        val index = menuDtoList.indexOfFirst { it.id == id }
+
+        menuDtoList.removeAt(index)
+
+        dataSource.saveMenuData(menuDtoList)
+
+        return true
+    }
 }
--- src/main/kotlin/domain/repository/MenuRepository.kt
+++ src/main/kotlin/domain/repository/MenuRepository.kt
@@ -1,4 +1,17 @@
 package domain.repository
 
-class MenuRepository {
+import domain.model.Menu
+
+interface MenuRepository {
+    /**메뉴 목록조회*/
+    fun getMenuList(): List<Menu>
+
+    /**메뉴 추가*/
+    fun addMenu(name: String, price: Int): Menu
+
+    /**메뉴 수정*/
+    fun updateMenu(id: Int, price: Int): Menu
+
+    /**메뉴 삭제*/
+    fun deleteMenu(id: Int): Boolean
 }
