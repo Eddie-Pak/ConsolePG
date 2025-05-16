0f1aad35f45fe2414b54c7d177f705e6a04fea75
Eddie-Pak <foalstn@naver.com>
2025-05-16 17:34:59 +0900

    feat: MenuViewModel 구현

--- src/main/kotlin/view/model/MenuViewModel.kt
+++ src/main/kotlin/view/model/MenuViewModel.kt
@@ -1,4 +1,14 @@
 package view.model
 
-class MenuViewModel {
+import domain.model.Menu
+import domain.repository.MenuRepository
+
+class MenuViewModel(private val repository: MenuRepository) {
+    fun getMenuList(): List<Menu> = repository.getMenuList()
+
+    fun addMenu(name: String, price: Int): Menu = repository.addMenu(name, price)
+
+    fun updateMenu(id: Int, price: Int): Menu = repository.updateMenu(id, price)
+
+    fun deleteMenu(id: Int): Boolean = repository.deleteMenu(id)
 }
