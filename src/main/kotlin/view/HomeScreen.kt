069d0eefd4280a30d449baf8b1811031f6f17243
Eddie-Pak <foalstn@naver.com>
2025-05-17 00:48:58 +0900

    feat: HomeScreen, MenuScreen getMenu 구현

--- src/main/kotlin/view/HomeScreen.kt
+++ src/main/kotlin/view/HomeScreen.kt
@@ -1,4 +1,18 @@
 package view
 
-class HomeScreen {
+import kotlin.system.exitProcess
+
+class HomeScreen(private val menuScreen: MenuScreen) : BaseScreen {
+    override fun display() {
+        println("Welcome")
+        println("[1] 메뉴관리 [0] 종료")
+    }
+
+    override fun handleInput() {
+        when (readlnOrNull()) {
+            "1" -> ConsoleController.currentScreen = menuScreen
+            "0" -> exitProcess(0)
+            else -> println("잘못된 선택")
+        }
+    }
 }
--- src/main/kotlin/view/MenuScreen.kt
+++ src/main/kotlin/view/MenuScreen.kt
@@ -1,4 +1,48 @@
 package view
 
-class MenuScreen {
+import view.model.MenuViewModel
+
+class MenuScreen(
+    private val viewModel: MenuViewModel,
+    private val homeScreen: HomeScreen? = null
+) : BaseScreen {
+    override fun display() {
+        println("======== 메뉴 관리 ========")
+        println("1. 메뉴확인 2. 메뉴추가 3. 메뉴수정 4. 메뉴삭제 0. 홈이동")
+        println("==========================")
+    }
+
+    override fun handleInput() {
+        when (readlnOrNull()) {
+            "0" -> {
+                println("==========================")
+                println("          홈 이동          ")
+                println("==========================")
+
+                homeScreen?.let { ConsoleController.currentScreen = it }
+            }
+            "1" -> getAllMenu()
+            else -> println("잘못된 입력")
+        }
+    }
+
+    private fun getAllMenu() {
+        println("\n======== 메뉴 목록 ========")
+
+        val menuList = viewModel.getMenuList()
+
+        if (menuList.isEmpty()) {
+            println("등로된 메뉴가 없습니다.")
+            println()
+        } else {
+            for (i in menuList.indices) {
+                val menu = menuList[i]
+
+                println("${i+1}. ${menu.name} - ${menu.price}원")
+            }
+
+            println("엔터키를 누르면 돌아갑니다.")
+            readlnOrNull()
+        }
+    }
 }
