5d3a090a20b50b07e68e00886761fdfa1ee00b29
Eddie-Pak <foalstn@naver.com>
2025-08-11 20:09:50 +0900

    #1 feat: MenuDataSource 코루티 변경 시작

--- src/main/kotlin/data/datasource/MenuDataSource.kt
+++ src/main/kotlin/data/datasource/MenuDataSource.kt
@@ -3,6 +3,7 @@ package data.datasource
 import data.dto.MenuDTO
 
 interface MenuDataSource {
+    // 코루틴으로 바꿔야함
     fun getMenuData(): List<MenuDTO>
 
     fun saveMenuData(menuDto: List<MenuDTO>): Boolean
